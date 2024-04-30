96a9267ee4f5268036014e201516cb721f3c4c14
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2024-04-30 23:54:44 +0600

    Successfully find a document

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 
-console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6dqvzgb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -27,12 +26,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const serviceCollection = client.db("carDoctor").collection("services");
+
+     app.get('/services', async (req, res) => {
+        const cursor = serviceCollection.find();
+        const services = await cursor.toArray();
+        res.send(services);
+     })
+     app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+
+        const options = {
+           
+            projection: {  title: 1, price: 1,service_id:1 },
+          }; 
+
+        const result = await serviceCollection.findOne(query,options);
+        res.send(result)
+     })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
