b018597c885a57c478337f5a083ff95e2663985d
Kibria-Rafi <kibriarafi.bd.natore@gmail.com>
2024-05-02 02:19:56 +0600

    get booking data from mongodb

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function run() {
     app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const options = { projection: { title: 1, price: 1, service_id: 1 } };
+      const options = { projection: { title: 1, price: 1, service_id: 1, img: 1 } };
 
       const result = await serviceCollection.findOne(query, options);
       res.send(result);
